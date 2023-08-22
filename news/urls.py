0f43677122a4f269aab3c75e22516334ce3351d6
Bruno Ferreira <contatobrunokye@gmail.com>
2023-08-22 17:51:30 -0300

    feat: requisito 08

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from news.views import home, news_details, categories_form, news_form
+from rest_framework import routers
+from news_rest.views import categories_view
+
+router = routers.DefaultRouter()
+router.register(r'categories', categories_view.CategoriesViewSet)
 
 urlpatterns = [
     path('', home, name='home-page'),
     path('news/<int:id>', news_details, name="news-details-page"),
     path('categories/', categories_form, name="categories-form"),
-    path('news/', news_form, name="news-form")
+    path('news/', news_form, name="news-form"),
+    path('api/', include(router.urls))
 ]
