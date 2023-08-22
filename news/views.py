417a0f129f32d986ca7478c64277e14957e8f990
Bruno Ferreira <contatobrunokye@gmail.com>
2023-08-22 15:26:37 -0300

    feat: requisito 05

--- news/views.py
+++ news/views.py
@@ -5,3 +5,8 @@ from news.models import News
 def home(request):
     news = News.objects.all()
     return render(request, 'home.html', {"news": news})
+
+
+def news_details(request, id):
+    news = News.objects.get(id=id)
+    return render(request, 'news_details.html', {"news": news})
